7a93b4293a6ae3401f5bf050e2efa227d27c698f
afaqahmedkhan <afaq.qwe@gmail.com>
2023-11-22 10:30:35 +0100

    practice: collection which is ordered and changeable

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,22 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+numbers = [1,2,31,1,1]
+
+print(numbers)
+
+print(numbers[0])
+
+print(len(numbers))
+
+numbers.append("Mangoes")
+
+print(numbers)
+
+numbers.insert(2, "Mangoes")
+print(numbers)
+
+print(numbers.remove("Mangoes"))
+print(numbers.remove("Mangoes"))
+print(numbers.sort())
+print(numbers)
+
