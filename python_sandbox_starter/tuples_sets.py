53e709e3b226201cea91a849b25737a1b41e4d24
afaqahmedkhan <afaq.qwe@gmail.com>
2023-11-22 11:04:32 +0100

    - Set is a collection which is unordered and unindexed. - Tuple is a collection which is ordered and unchangeable

--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -1,5 +1,20 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
  
+fruits = ("apples", "banana")
 
+#unchangeable
+#fruits[0] = "oranges"
+
+#del fruits
+
+print(fruits)
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+
+fruit_set = {"apples", "oranges", "mangoes"}
+
+print("apples" in fruit_set)
+
+fruit_set.clear()
+
+print(fruit_set)
