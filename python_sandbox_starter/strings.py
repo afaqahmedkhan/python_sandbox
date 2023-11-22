1e62d62afd03a35a1e7f1ec4523aa8e6c329af29
afaqahmedkhan <afaq.qwe@gmail.com>
2023-11-22 10:24:02 +0100

    strings, isalpha returns false if space, startswith returns booleans, len(s) arg

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -3,4 +3,29 @@
 
 # String Formatting
 
+name = "Brad"
+
+age = 24
+
+print("my name is " + name + " and age is " + str(age))
+
+print('my name is {name} and age is {age}'.format(name=name, age=age))
+
+print(f"my name is {name} and age is {age}") # only works 3.6 and above
+
 # String Methods
+
+s = "helloworld"
+
+print(s.capitalize())
+print(s.upper())
+print(s.lower())
+print(len(s))
+print(s.replace("world", "everyone"))
+print(s.count("o"))
+print(s.startswith("h"))
+print(s.find("r"))
+print(s.isalpha())
+print(s.isnumeric())
+print(s.split("w"))
+
